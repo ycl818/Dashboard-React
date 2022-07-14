d09ca42da44e662dde4b2521e224532f2e699382
yichien <kevinyichienlee@gmail.com>
2022-07-15 02:06:46 +0800

    added routers

--- src/App.js
+++ src/App.js
@@ -36,8 +36,40 @@ const App = () => {
         <div className={
           `dark-bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
         }>
-
+          <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+            Navbar
+          </div>
         </div>
+
+        <div>
+          <Routes>
+            {/* Dashboard */}
+            <Route path='/' element="ECommerce" />
+            <Route path='/ecommerce' element="ECommerce" />
+
+            {/* Pages */}
+            <Route path='/orders' element="Orders" />
+            <Route path='/employees' element="Empolyees" />
+            <Route path='/customers' element="Customers" />
+
+            {/* Apps */}
+            <Route path='/kanban' element="Kanban" />
+            <Route path='/editor' element="Editor" />
+            <Route path='/calendar' element="Calendar" />
+            <Route path='/color-picker' element="ColorPicker" />
+
+            {/* Charts */}
+            <Route path='/line' element="Line" />
+            <Route path='/area' element="Area" />
+            <Route path='/bar' element="Bar" />
+            <Route path='/pie' element="Pie" />
+            <Route path='/financial' element="Financial" />
+            <Route path='/color-mapping' element="ColorMapping" />
+            <Route path='/pyramid' element="Pyramid" />
+            <Route path='/stacked' element="Stacked" />
+          </Routes>
+        </div>
+
       </div>
     </BrowserRouter>
   )
