055583664be82e9cf87cea42d5cfef99c053c0e4
yichien <kevinyichienlee@gmail.com>
2022-07-15 01:07:27 +0800

    added data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSetting } from 'react-icons/fi';
+import { TooltipComponent} from '@syncfusion/ej2-react-popups';
+
+
+import './App.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <div className='flex relative'>hi</div>
+    </BrowserRouter>
   )
 }
 
