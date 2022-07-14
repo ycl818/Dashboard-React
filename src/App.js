ef50e85a04037b46d12a3bd20418cf12b7aca764
yichien <kevinyichienlee@gmail.com>
2022-07-15 02:30:41 +0800

    fixed components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent} from '@syncfusion/ej2-react-popups';
 
-
+import { Navbar, Footer, Sidebar, ThemeSettings} from './components';
+import { Ecommerce, Orders, Calendar, Empolyees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorMapping, ColorPicker, Editor} from './pages';
 import './App.css'
 
 const App = () => {
